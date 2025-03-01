ab223cc4be4ea08d4bd3eae81d524fae19a7b727
raihan51674 <mdraihan51674@gmail.com>
2025-03-01 12:29:58 +0600

    first commit

---
+++ js/back.js
@@ -0,0 +1,4 @@
+document.getElementById("backDesk").addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+  
---
+++ js/blog.js
@@ -0,0 +1,4 @@
+document.getElementById("toBlog").addEventListener("click", function () {
+    window.location.href = "blog.html";
+  });
+  
